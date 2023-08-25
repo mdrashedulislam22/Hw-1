bd2c4f83a7e36569215f0fce4bb8f1ebcf895229
mdrashedulislam22 <121747355+mdrashedulislam22@users.noreply.github.com>
2023-08-26 01:39:22 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+//string practice
+   
+    echo " <h2 style= color:green;>Hi Guys!</h2><br/>";
+    echo " <h2 style=color:green;>I am Md Rashedul islam</h2><br/>";
+    echo " <p><b>From</b> Chakaria cox's bazar <b>dawraya hadithc</b> completed from al-jamia al-islamia jomiriya kasemul ulum patiya
+    <b>And</b> completed web design from creative it institute <b>Batch-No</b> 2212 <b>Now</b> Started web development with you <b> I hope </b> we are will be end the cours with fantasticly insaallah</p> <br/>";
+
+// number practice
+ $number = 100;
+ $number2 = 100;
+    echo "100 + 100 = ".  $number + $number2 ."<br/><br/>";
+//variable declaration practice
+$mojar_golpo = "মিষ্ঠি";
+    echo "একটা <u>$mojar_golpo</u> মেয়ে বাজারে গেলো <u>$mojar_golpo</u> কিনতে। <u>$mojar_golpo</u> কিনতে গিয়ে সে বাজারের মধ্যে <u>$mojar_golpo</u> একটা ছেলের সঙ্গে ধাক্কা খেলো।
+    ধাক্কা খেয়ে তার হাত থেকে সব <u>$mojar_golpo</u> পরে গেলো। এতে <u>$mojar_golpo</u> মেয়েটা খুব রেগে গেলো। কিন্তু ছেলেটা মেয়েটাকে আসস্ত করলো এবং <u>$mojar_golpo</u>  <u>$mojar_golpo</u> কথা বলে <u>$mojar_golpo</u> র দোকানে নিয়ে গেলো। সেখান থেকে মেয়েটিকে <u>$mojar_golpo</u> কিনে দিলো। এভাবে দুজনের মধ্যে <u>$mojar_golpo</u> একটা সম্পর্ক সৃষ্টি হলো। <br/>";
+?>
